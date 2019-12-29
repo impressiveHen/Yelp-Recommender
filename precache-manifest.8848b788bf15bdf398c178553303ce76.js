d86bd1157316be8f3ee23eec8d05c4cb7770fdab
impressiveHen <b02505002@gmail.com>
2019-12-29 15:08:07 -0800

    Updates

---
+++ precache-manifest.8848b788bf15bdf398c178553303ce76.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e41d9a1d07a232cedb65e3aed9960d3",
+    "url": "/Yelp-Recommender/index.html"
+  },
+  {
+    "revision": "dcc2b8281c911f0ac00a",
+    "url": "/Yelp-Recommender/static/css/main.5a4ee508.chunk.css"
+  },
+  {
+    "revision": "1b42777b0b87da2f66b3",
+    "url": "/Yelp-Recommender/static/js/2.07052e59.chunk.js"
+  },
+  {
+    "revision": "c38e7637c143d0473f3176f1c30b4c2e",
+    "url": "/Yelp-Recommender/static/js/2.07052e59.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dcc2b8281c911f0ac00a",
+    "url": "/Yelp-Recommender/static/js/main.0b9d2f45.chunk.js"
+  },
+  {
+    "revision": "e686b794fcc41747172f",
+    "url": "/Yelp-Recommender/static/js/runtime-main.710b6b33.js"
+  },
+  {
+    "revision": "8c5dfcbd382070d0baeafbc51a30f10b",
+    "url": "/Yelp-Recommender/static/media/yelp-black.8c5dfcbd.jpg"
+  },
+  {
+    "revision": "6b87688b8bcea7f0477b2d9e309e38bc",
+    "url": "/Yelp-Recommender/static/media/yelp_image.6b87688b.png"
+  }
+]);
